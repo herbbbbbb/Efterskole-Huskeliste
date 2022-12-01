f9c796978da17269392d400236c60da8bc49f6d5
Herbert <Herbert.bjerre@gmail.com>
2022-12-01 23:43:17 +0100

    Initial Commit

---
+++ Efterskole Huskeliste/Efterskole_HuskelisteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Efterskole_HuskelisteApp.swift
+//  Efterskole Huskeliste
+//
+//  Created by Herbert Lind Bjerre on 01/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Efterskole_HuskelisteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
